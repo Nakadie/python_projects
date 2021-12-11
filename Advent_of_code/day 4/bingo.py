60756facdc8d16b6720e1179c420f5503e35fd2a
Nakadie <77523003+Nakadie@users.noreply.github.com>
2021-12-11 17:46:41 +0900

    day 4 part 1 finished

--- Advent_of_code/day 4/bingo.py
+++ Advent_of_code/day 4/bingo.py
@@ -1,19 +1,132 @@
 import collections
-txt = open('D:\python_projects\Advent_of_code\day 4\puzzle.txt', 'r') 
+import numpy as np
+from numpy.core.records import array
+txt = open('D:\python_projects\Advent_of_code\day 4\puzzle.txt', 'r')
 txt = txt.read().splitlines()
+boardlist = open('D:\python_projects\Advent_of_code\day 4\puzzle2.txt', 'r')
+boardlist = boardlist.read().splitlines()
 
 drawn_nums = [txt[0]]
-boards = []
-while True:
-    temp = []
-    while len(temp) != 5:
-        for i in txt:
-            if i == '':
-                pass
-            else:
-                temp.append(i)
+drawn_nums = drawn_nums[0].split(',')
+drawn_nums = [int(x) for x in drawn_nums]
+#drawn_nums = [92, 3, 88, 13, 50]
 
 
+def get_boards():
+    temp = [x for x in boardlist if x != '']
+    boards = []
+    board = []
+    for i in temp:
+        if len(board) != 5:
+            board.append([int(k) for k in i.split()])
+        if len(board) == 5:
+            boards.append(board)
+            board = []
+    return boards
+
+def draw_and_update(drawn_nums):
+    y = 0
+    boards = np.array(get_boards(), dtype=object)
+    for num in drawn_nums:
+        boards = np.where(boards == num, 'x', boards)
+        if check_bingo(boards) == True:
+            print(num)
+            break
+        else:
+            y += 1
+            print(f'no count == {y}')
+                
+                
+                
+def check_bingo(boards):
+    possible_bingo = [
+                np.array([
+                ['x', '', '', '', ''],
+                ['x', '', '', '', ''],
+                ['x', '', '', '', ''],
+                ['x', '', '', '', ''],
+                ['x', '', '', '', ''],
+            ]),
+            np.array([
+                ['', 'x', '', '', ''],
+                ['', 'x', '', '', ''],
+                ['', 'x', '', '', ''],
+                ['', 'x', '', '', ''],
+                ['', 'x', '', '', ''],
+            ]),
+            np.array([
+                ['', '', 'x', '', ''],
+                ['', '', 'x', '', ''],
+                ['', '', 'x', '', ''],
+                ['', '', 'x', '', ''],
+                ['', '', 'x', '', ''],
+            ]),
+            np.array([
+                ['', '', '', 'x', ''],
+                ['', '', '', 'x', ''],
+                ['', '', '', 'x', ''],
+                ['', '', '', 'x', ''],
+                ['', '', '', 'x', ''],
+            ]),
+            np.array([
+                ['', '', '', '', 'x'],
+                ['', '', '', '', 'x'],
+                ['', '', '', '', 'x'],
+                ['', '', '', '', 'x'],
+                ['', '', '', '', 'x'],
+            ]),
+            np.array([
+                ['x', 'x', 'x', 'x', 'x'],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+            ]),
+            np.array([
+                ['', '', '', '', ''],
+                ['x', 'x', 'x', 'x', 'x'],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+            ]),
+            np.array([
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['x', 'x', 'x', 'x', 'x'],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+            ]),
+            np.array([
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['x', 'x', 'x', 'x', 'x'],
+                ['', '', '', '', ''],
+            ]),
+            np.array([
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['x', 'x', 'x', 'x', 'x'],
+            ])
+            ]
+    boards = np.array(boards)
+    for i in boards:
+        for x in possible_bingo:
+            check = (i == x)
+            occurrences = np.count_nonzero(check == True)
+            if occurrences == 5:
+                final = np.sum(np.where(i == 'x', 0, i))
+                return True
+                
     
-    
-print(txt)
+            
+           
+
+
+boards = get_boards()
+
+draw_and_update(drawn_nums)
+
+
