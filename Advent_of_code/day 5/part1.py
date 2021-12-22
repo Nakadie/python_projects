06b15bbcf5b10d42f8d4410384be339a869e22c3
Nakadie <77523003+Nakadie@users.noreply.github.com>
2021-12-22 14:28:17 +0900

    Day 5 parts 1 and 2 complete

--- Advent_of_code/day 5/part1.py
+++ Advent_of_code/day 5/part1.py
@@ -4,12 +4,10 @@
 
 # To avoid the most dangerous areas, you need to determine the number of points where at least two lines overlap. In the above example, this is anywhere in the diagram with a 2 or larger - a total of 5 points.
 
-#Unfortunately, considering only horizontal and vertical lines doesn't give you the full picture; you need to also consider diagonal lines.
-
+# Consider only horizontal and vertical lines. At how many points do at least two lines overlap?
 
 import collections
 import numpy as np
-from numpy.lib.twodim_base import diag
 txt = open('D:\python_projects\Advent_of_code\day 5\puzzle.txt', 'r')
 txt = txt.read().splitlines()
 
@@ -17,19 +15,14 @@ txt = [x.split() for x in txt]
 start = [x[0].split(',') for x in txt]
 finish = [x[2].split(',') for x in txt]
 
-
 vert_lines = []
 hori_lines = []
-diag_lines = []
-diag_lines2 = []
 
 for i in range(len(start)):
     if (int(start[i][0]) == int(finish[i][0])):
-        vert_lines.append([int(start[i][0]), (sorted([int(finish[i][1]), int(start[i][1])]))])
+        vert_lines.append([int(start[i][0]), (sorted([int(finish[i][1]), int(start[i][1 ])]))])
     elif (int(start[i][1]) == int(finish[i][1])):
         hori_lines.append([int(start[i][1]), (sorted([int(finish[i][0]), int(start[i][0])]))])
-    else:
-        diag_lines.append([start[i], finish[i]])
 
 graph = np.zeros((1000, 1000))
 
@@ -41,35 +34,9 @@ for i in range(len(vert_lines)):
     for x in range((vert_lines[i][1][0]), ((vert_lines[i][1][1])+1)):
         graph[x][vert_lines[i][0]] +=1
 
-for i in range(len(diag_lines)):
-    start_x = int(diag_lines[i][0][0])
-    start_y = int(diag_lines[i][0][1])
-    end_x = int(diag_lines[i][1][0])
-    end_y = int(diag_lines[i][1][1])
-    #print(f'[{start_x}, {start_y}] , [{end_x}, {end_y}]')
-
-    if start_x > end_x and start_y > end_y:
-        for x, y in zip(reversed(range(end_x, start_x + 1)), reversed(range(end_y, start_y + 1))):
-            graph[y][x] += 1
-            #print(x, y)
-    elif start_x < end_x and start_y < end_y:
-        for x, y in zip(range(start_x, end_x + 1), range(start_y, end_y + 1)):
-            graph[y][x] += 1   
-            #print(x, y)
-    elif start_x < end_x and start_y > end_y:
-        for x, y in zip((range(start_x, end_x + 1)), reversed(range(end_y, start_y + 1))):
-            graph[y][x] += 1
-            #print(x, y)
-    elif start_x > end_x and start_y < end_y:
-        for x, y in zip(reversed(range(end_x, start_x + 1)), range(start_y, end_y + 1)):
-            graph[y][x] += 1
-            #print(x, y)
-        
-        
-
-        
 occurrences = np.count_nonzero(graph >= 2)      
 
 print(occurrences)
+
 
 
