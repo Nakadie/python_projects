5d86a0996b250abb73f158e94d21291b02dd70aa
Nakadie <77523003+Nakadie@users.noreply.github.com>
2022-02-15 20:53:17 +0900

    backend near complete

--- Projects/Hospital tool/Hospital_Patient_Tool.py
+++ Projects/Hospital tool/Hospital_Patient_Tool.py
@@ -1,14 +1,14 @@
 """
 Hospital patient data base tool used to track patients and ailments.
 """
-  
+initializetxt = open('patient_db.txt', "a")
 
 class Patient(object):
     """
     A Patient is a person who has come to see the doctor. 
     they have name, age, weight, height, gender
     """
-    def __init__(self, lname, fname, age, weight, height, bloodtype):
+    def __init__(self, patnum, lname, fname, age, weight, height, bloodtype):
         """
         Initializes a patient with patient number, last name, firstname, age, weight, height, bloodtype
         """
@@ -19,17 +19,19 @@ class Patient(object):
         self.lname = lname.capitalize()
         self.bloodtype = bloodtype
         self.age = age
-        self.patnum = self.get_patnum()
+        self.patnum = patnum #self.get_patnum()
+        # self.patlist = self.get_all_pats()
 
     def get_patnum(self):
-        ################
-        # not finished
-        ################
         """
         gets the new patient number.
         """
-        patdat = [i.split() for i in database().patient_strs]
-        print(patdat)
+        database()
+        if database().patient_strs == []:
+            return 1
+        else:
+            return int(database().patient_strs[-1][0]) + 1
+        
        
 
 
@@ -38,6 +40,17 @@ class Patient(object):
         turns a patient class into a string for storage in the txt file.
         """
         return f"{self.patnum} {self.lname} {self.fname} {self.age} {self.weight} {self.height} {self.bloodtype} "
+        
+    @classmethod
+    def get_all_pats(cls):
+        """
+        get all the patients from the patient_db.txt and initialize them to a class and put in a list.
+        """
+        all_patients = []
+        args = database().patient_strs
+        for i in args:
+            all_patients.append(cls(*i))
+        return all_patients
 
 class database(object):
     """
@@ -52,7 +65,7 @@ class database(object):
         """
         Get all the patients from the patient_db.txt
         """
-        txt = open(self.filename, 'a+')
+        txt = open(self.filename, 'r')
         patient_strs = txt.read().splitlines()
         patient_strs = [x.split() for x in patient_strs]
         return patient_strs
@@ -71,25 +84,29 @@ class database(object):
             if len(data) > 0:
                 file_object.write("\n")
             file_object.write(patient.to_string())
-    
-    def create_list(self):
-        """
-        Creates the list that lists patient info in individual strings.
-        """
-
-        data = [x.split() for x in self.patient_strs]
-        return data
 
     
 
+    
+    # def create_list(self):
+    #     """
+    #     Creates the list that lists patient info in individual strings.
+    #     """
+    #     pass
+    #     # data = [x.split() for x in self.patient_strs]
+    #     # return data
+
+    
 
 
-patients_list = [Patient('Brady', 'tom', 225, 75, 'O+', 34)]
+
+#patients_list = [Patient('Brady', 'tom', 225, 75, 'O+', 34)]
 
 
 # Patient('Brady', 'tom', 225, 75, 'O+', 34).get_patnum()
-database().initialize_patients()
+# database().initialize_patients()
 # database().add_db((patients_list[0]))
 # database().create_list()
-print(database().patient_strs)
+# print(database().patient_strs)
+print(Patient.get_all_pats())
 
