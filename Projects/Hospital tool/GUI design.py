d1da40ade83552f8dc53832be1f75f22282dee92
Nakadie <77523003+Nakadie@users.noreply.github.com>
2022-02-23 15:05:38 +0900

    update test branch

--- Projects/Hospital tool/GUI design.py
+++ Projects/Hospital tool/GUI design.py
@@ -108,7 +108,7 @@ class MainWindow(QMainWindow):
         self.btn2.move(150, 50)   
         self.btn2.clicked.connect(self.goto_search)
         
-        self.statusBar()
+        
         
         self.setGeometry(300, 300, 290, 150)
         self.setWindowTitle('Patient Database')
@@ -134,6 +134,7 @@ class New_Patient_window(QDialog):
         super().__init__()
         
         self.initUI()
+        self.display_pat = Display_patient()
         
 
     def initUI(self):  
@@ -143,8 +144,10 @@ class New_Patient_window(QDialog):
         self.hgt = QLineEdit()
         self.weight = QLineEdit()
         self.blood = QComboBox()
+        self.patnum = str(database().get_patnum())
         self.sex = 's'
         layout = QFormLayout()
+
     
         
         hbox = QHBoxLayout
@@ -155,7 +158,7 @@ class New_Patient_window(QDialog):
         self.setWindowTitle('New Patient Entry')
         self.blood.addItems(['A+', 'O+', 'B+', 'AB+', 'A-', 'O-', 'B-', 'AB-'])
 
-     
+        layout.addRow(("Patient #:"), QLabel(self.patnum))
         layout.addRow(("Last Name:"), self.lname)
         layout.addRow(("First Name:"), self.fname)
         layout.addRow(("Age:"), self.age)
@@ -172,11 +175,23 @@ class New_Patient_window(QDialog):
         
         
         self.setLayout(layout)
-        print(self.lname.text)
+        
 
     def create_pat(self):
-        new_pat = Patient(database().get_patnum(), self.lname.text(), self.fname.text(), self.age.text(), self.weight.text(), self.hgt.text(), self.blood.currentText())
+        new_pat = Patient(self.patnum, self.lname.text(), self.fname.text(), self.age.text(), self.weight.text(), self.hgt.text(), self.blood.currentText())
         database().add_db(new_pat)
+
+        self.display_pat.patnum.setText(self.patnum)
+        self.display_pat.lname.setText(self.lname.text())
+        self.display_pat.fname.setText(self.fname.text()) 
+        self.display_pat.age.setText(self.age.text())  
+        self.display_pat.hgt.setText(self.hgt.text()) 
+        self.display_pat.weight.setText(self.weight.text()) 
+        self.display_pat.blood.setText(self.blood.currentText()) 
+        self.sex = QLabel()
+
+        self.display_pat.show()
+        
     
         self.close()
 
@@ -189,7 +204,8 @@ class Search_window(QDialog):
     """
     def __init__(self):
         super().__init__()
-        
+        self.display_pat = Display_patient()
+
         self.initUI()
         
     def initUI(self):  
@@ -200,8 +216,13 @@ class Search_window(QDialog):
         self.box = QDialogButtonBox(backbtn)
         self.search1 = QPushButton('Search')
         self.search2 = QPushButton('Search')
+        self.namerr = QLabel()
+        self.numerr = QLabel()
+
+        layout.addRow('', self.namerr)
         layout.addRow(("Last Name:"), self.lname)
         layout.addRow('', self.search1)
+        layout.addRow('', self.numerr)
         layout.addRow(("Patient #:"), self.patnum)
         layout.addRow('', self.search2)
         layout.addRow('', self.box)
@@ -215,20 +236,68 @@ class Search_window(QDialog):
         self.search2.clicked.connect(self.patnum_search)
 
     def name_search(self):
-        names = [x.lname for x in Patient.get_all_pats()]
-        for i in names:
-            if i == self.lname.text().capitalize():
+        names = Patient.get_all_pats()
+        for Patient.lname in names:
+            if Patient.lname == self.lname.text().capitalize():
                 print('found')
-        self.close()
+            else:
+                self.namerr.setText(self.lname.text().capitalize() + ': Not Found')
+        
     
     def patnum_search(self):
         numbers = [x.patnum for x in Patient.get_all_pats()]
         for i in numbers:
             if int(i) == int(self.patnum.text()):
-                print('found')
-        self.close()
-        
+                self.display_pat.patnum.setText(str(int(i)-1))
+                self.display_pat.lname.setText((Patient.get_all_pats())[(int(i)-1)].lname)
+                self.display_pat.fname.setText((Patient.get_all_pats())[(int(i)-1)].fname) 
+                self.display_pat.age.setText((Patient.get_all_pats())[(int(i)-1)].age)  
+                self.display_pat.hgt.setText((Patient.get_all_pats())[(int(i)-1)].height) 
+                self.display_pat.weight.setText((Patient.get_all_pats())[(int(i)-1)].weight) 
+                self.display_pat.blood.setText((Patient.get_all_pats())[(int(i)-1)].bloodtype) 
+                self.sex = QLabel()
+                
+                self.close()
+                self.display_pat.show()
+            else:
+                self.numerr.setText('Patient #' + self.patnum.text() + ': Not Found')
 
+class Display_patient(QWidget):
+    """
+    This "window" is a QWidget. If it has no parent, it
+    will appear as a free-floating window as we want.
+    This window will display patient information.
+    """
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+        
+    def initUI(self): 
+        self.patnum = QLabel()
+        self.lname = QLabel()
+        self.fname = QLabel() 
+        self.age = QLabel() 
+        self.hgt = QLabel() 
+        self.weight = QLabel() 
+        self.blood = QLabel() 
+        self.sex = QLabel()
+        layout = QFormLayout()
+    
+        
+       
+        self.setLayout(layout)
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('Patient info')
+
+        layout.addRow(("Patient #:"), self.patnum)
+        layout.addRow(("Last Name:"), self.lname)
+        layout.addRow(("First Name:"), self.fname)
+        layout.addRow(("Age:"), self.age)
+        layout.addRow(("Height (cm):"), self.hgt)
+        layout.addRow(("Weight (kgs):"), self.weight)
+        layout.addRow(("Blood Type:"), self.blood)
         
 def main():
     
@@ -239,30 +308,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-    
-
-    
-    def create_list(self):
-        """
-        Creates the list that lists patient info in individual strings.
-        """
-        pass
-        # data = [x.split() for x in self.patient_strs]
-        # return data
-
-    
-
-
-
-#patients_list = [Patient('Brady', 'tom', 225, 75, 'O+', 34)]
-
-
-# Patient('Brady', 'tom', 225, 75, 'O+', 34).get_patnum()
-# database().initialize_patients()
-# database().add_db((patients_list[0]))
-# database().create_list()
-# print(database().patient_strs)
-
-
