c025fb4a62180be5859186e873ff859a170277f7
Nakadie <77523003+Nakadie@users.noreply.github.com>
2022-06-19 17:48:11 +0900

    leetcode 57 almost finished

--- Puzzles/LeetCode/blind 72/57. Insert Interval.py
+++ Puzzles/LeetCode/blind 72/57. Insert Interval.py
@@ -1,27 +1,31 @@
 class Solution:
-    def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
+    def insert(self, intervals: list[list[int]], new: list[int]) -> list[list[int]]:
         buffer = []
         new_ints = []
-        low = 0
-        for i in intervals:
-            if i[0] < newInterval[0] and i[1] > newInterval[0]:
-                buffer.append(i[0])
-                low = i[0]
-                for j in intervals:
-                    if j[0] < newInterval[1] < j[1]:
-                        buffer.append(newInterval[1])
-                        
-            elif low == 0:
-                buffer.append(i[0])
-                buffer.append(i[1])
+        if intervals == []:
+            print([new])
+            return [new]
+        for i in range(len(intervals)):
+            if intervals[i][0] <  new[0] and intervals[i][1] > new[0]:
+                buffer.append(intervals[i][0])
+                if intervals[(i+1)][0] > new[1]:
+                    buffer.append(new[1])
+            elif intervals[i][0] == new[1]:
+                buffer.append(intervals[i][1])
             
-            # elif low > 1 and i[0] > newInterval[1]:
-            #     buffer.append(i[0])
-            #     buffer.append(i[1])
-
-        print(buffer)
-Solution().insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8])
-Solution().insert([[1,3],[6,9]], [2, 5])
+            elif intervals[i][0] > new[0] and intervals[i][1] < new[1]:
+                pass
+            else:
+                buffer.append(intervals[i][0])
+                buffer.append(intervals[i][1])
+                
+        for i in range(0,len(buffer), 2):
+            new_ints.append([buffer[i],buffer[(i+1)]])
+        
+        return new_ints
+# Solution().insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8])
+# Solution().insert([[1,3],[6,9]], [2, 5])
+Solution().insert([], [100, 200])
 # Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
 # Output: [[1,2],[3,10],[12,16]]
 # Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
